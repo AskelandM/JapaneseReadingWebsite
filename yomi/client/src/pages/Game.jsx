7219555d17d29f07a96829bb1390b29a851c397f
ssihala <ssihala@tutanota.com>
2025-04-18 11:38:34 -0400

    Fix game for custom lessons

--- yomi/client/src/pages/Game.jsx
+++ yomi/client/src/pages/Game.jsx
@@ -1,15 +1,37 @@
 import Concentration from "../components/Concentration";
 import { useParams } from "react-router-dom";
 import { useLocation } from "react-router";
+import supabase from "../supabaseclient";
+import React from "react";
 
 const Game = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const lesson = searchParams.get("lesson");
+  const lessonID = searchParams.get("lesson");
+  const [lessonTitle, setLessonTitle] = React.useState("");
+
+  React.useEffect(() => {
+    const fetchTitle = async (id) => {
+      const { data, error } = await supabase
+        .from("lessons")
+        .select("title")
+        .eq("id", id)
+        .single();
+      if (error) {
+        console.error("Error fetching lesson title:", error);
+        return "";
+      }
+      return data.title;
+    };
+    fetchTitle(lessonID).then((title) => {
+      setLessonTitle(title);
+    });
+  });
+
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Lesson {lesson}</h1>
-      <Concentration lesson={lesson} />
+      <h1 style={{ textAlign: "center" }}>{lessonTitle}</h1>
+      <Concentration lessonID={lessonID} />
     </div>
   );
 };
