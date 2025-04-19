50df2af59f80dd914d5d5c505c679cebeed63122
ssihala <ssihala@tutanota.com>
2025-04-19 17:09:50 -0400

    Clean up files

--- yomi/client/src/data/flashcards 6.js
+++
@@ -1,7 +0,0 @@
-const kanjiDictionary = [
-    {id: 1, front: 'one', back: 'ichi'},
-    {id: 2, front: 'two', back: 'ni'},
-    {id: 3, front: 'three', back: 'san'},
-    {id: 4, front: 'four', back: 'shi'},
-    {id: 5, front: 'five', back: 'go'},
-];
