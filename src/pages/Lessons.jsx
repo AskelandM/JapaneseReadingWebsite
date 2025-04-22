222e8206282cbdef6b32a47ccb0c59957735c4cd
JagerRager <rodriguezjuan@ufl.edu>
2025-04-22 12:05:52 -0400

    remove overflow lock in lessons.jsx

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -25,15 +25,6 @@ const Lessons = () => {
 
     fetchData();
   }, []);
-
-  React.useEffect(() => {
-    const originalOverflow = document.body.style.overflow;
-    document.body.style.overflow = "hidden";
-  
-    return () => {
-      document.body.style.overflow = originalOverflow;
-    };
-  }, []);
   
   return (
     <div className="lessons-container">
