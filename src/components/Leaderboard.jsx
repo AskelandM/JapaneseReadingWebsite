0362f5652f82436b8ab25c1184288c61a9eef9fd
JagerRager <rodriguezjuan@ufl.edu>
2025-04-22 12:47:39 -0400

    fixed website wide scrolling ability, enabled now

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -43,11 +43,6 @@ const Leaderboard = () => {
 
     correctPoints();
 
-    const originalOverflow = document.body.style.overflow;
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = originalOverflow;
-    };
   }, []);
 
   return (
